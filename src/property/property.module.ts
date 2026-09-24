5e9cc05c6b75e2310e09c9e96abb8ed2e27e5bda
Piyush Gupta <guptapiyush592k@gmail.com>
2026-09-25 00:04:50 +0530

    logger middleware added

--- src/property/property.module.ts
+++ src/property/property.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PropertyController } from './propert.controller.js';
 import { PropertyService } from './property.service.js';
+import { LoggerMiddleware } from '../common/middleware/logger.middleware.js';
 
 @Module({
-    exports:[],
-    controllers:[PropertyController],
+    exports: [],
+    controllers: [PropertyController],
     providers: [PropertyService],
 })
-export class PropertyModule {}
+export class PropertyModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes(PropertyController)
+
+    }
+}
