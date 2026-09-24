584389e54eeffc752d3e5077ba9459fbaa2a17dd
Piyush Gupta <guptapiyush592k@gmail.com>
2026-09-25 00:14:38 +0530

    basic guard implemented

--- src/property/propert.controller.ts
+++ src/property/propert.controller.ts
@@ -1,48 +1,50 @@
-import { Body, Controller, Get, Param, Post, Query, Patch, Delete, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query, Patch, Delete, HttpCode, HttpStatus, UseGuards } from "@nestjs/common";
 import { PropertyService } from "./property.service.js";
 import { CreatePropertyDto } from "./dto/create-property.dto.js";
 import { UpdatePropertyDto } from "./dto/update-property.dto.js";
 import { PropertyQueryDto } from "./dto/property-query.dto.js";
+import { ApiKeyGuard } from "../common/guards/api-key.guard.js";
 
+@UseGuards(ApiKeyGuard)
 @Controller('properties')
-export class PropertyController{
+export class PropertyController {
 
-    constructor(private readonly propertyService:PropertyService){}
+    constructor(private readonly propertyService: PropertyService) { }
 
     @Get()
     getAllProperties(
         @Query() query: PropertyQueryDto
-    ){
+    ) {
         return this.propertyService.getAllProperties(query)
     }
 
     @Get(':id')
     getProperty(
-      @Param('id') id: string
+        @Param('id') id: string
     ) {
-      return this.propertyService.getProperty(id);
+        return this.propertyService.getProperty(id);
     }
 
     @Post()
     createProperty(
         @Body() body: CreatePropertyDto
-    ){
+    ) {
         return this.propertyService.addProperty(body)
     }
 
     @Patch(':id')
     updateProperty(
-    @Param('id') id: string,
-    @Body() body: UpdatePropertyDto,
+        @Param('id') id: string,
+        @Body() body: UpdatePropertyDto,
     ) {
-    return this.propertyService.updateProperty(id, body)
+        return this.propertyService.updateProperty(id, body)
     }
 
     @Delete(':id')
     @HttpCode(HttpStatus.NO_CONTENT)
     deleteProperty(
         @Param('id') id: string,
-    ){
+    ) {
         return this.propertyService.deleteProperty(id)
     }
 }
